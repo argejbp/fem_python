ba3a062905585a6bbc068d7aa171fbc7296fc64d
Argenis <45941131+argejbp@users.noreply.github.com>
2021-02-02 16:42:41 -0400

    Create fem_solution.py

--- fem_solution.py
+++ fem_solution.py
@@ -1,12 +1,8 @@
 import numpy as np 
 import scipy as sc
 
-def steady_state_solution(KG, FG, active_nodes):
-    sol = np.linealg.solve(KG[active_nodes[:, None], active_nodes], FG[active_nodes, :])
+def steady_state_solution(KG, FG):
+    sol = np.linalg.inv(KG).dot(FG)
     return sol
 
-def modal_solver(KG, MG, active_nodes):
-    MGi = np.linealg.inv(MG[active_nodes[:, None], active_nodes])
-    A = KG[active_nodes[:, None], active_nodes]*MGi
-    v, d = sc.linalg.eig(A)
-    return v, d
+
