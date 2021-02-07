6b89a58d68af20457d19e5ee0cb54d8eb4057b09
Argenis <45941131+argejbp@users.noreply.github.com>
2021-02-07 15:15:41 -0400

    Update boundary_conditions_1d_elements.py

--- boundary_conditions_1d_elements.py
+++ boundary_conditions_1d_elements.py
@@ -9,28 +9,29 @@ def thermal_boundary_conditions(bc, Elements, KG, FG):
         value2 = bc[i,3]
 
         if tb == 1:     #Fixed temperature (value1 = Temp)
-            node = bc[i, 1] - 1
+            node = int(bc[i, 1]) - 1
             KG[node, node] = KG[node, node]*1E+10
             FG[node, 0] = KG[node, node]*value1
 
         elif tb == 2:   #Heat Flow (value1 = Heat Flux; value2 = Area)
-            node = bc[i, 1] - 1
-            FG[node, 0] = value1*value2
+            node = int(bc[i, 1]) - 1
+            FG[node, 0] = FG[node, 0] + value1*value2
 
-        elif tb == 3:   #Heat convection (value1 = Temp; value2 = Conv. Coeff, value3 = Area)
-            node = bc[i, 1] - 1
+        elif tb == 3:   #Heat convection (value1 = Conv. Coeff; value2 = Temp, value3 = Area)
+            node = int(bc[i, 1]) - 1
             value3 = bc[i, 4]
 
             KG[node, node] = KG[node, node] + value1*value3
             FG[node, 0] = FG[node, 0] + value1*value2*value3
 
-        elif tb == 4:   #Heat generation (value1 = Heat generation; value2 = Volume)
-            element = bc[i, 3] - 1
-            nodei = Elements[element, 1] - 1
-            nodej = Elements[element, 2] - 1
+        elif tb == 4:   #Heat generation (value1 = Heat generation; value2 = Lenght of element; value3 = Area)
+            element = int(bc[i, 1]) - 1
+            nodei = int(Elements[element, 1]) - 1
+            nodej = int(Elements[element, 2]) - 1
+            value3 = bc[i, 4]
 
-            FG[nodei, 0] = FG[nodei, 0] + value1*value2/2
-            FG[nodej, 0] = FG[nodej, 0] + value1*value2/2
+            FG[nodei, 0] = FG[nodei, 0] + value1*value2/2*value3
+            FG[nodej, 0] = FG[nodej, 0] + value1*value2/2*value3
 
         else:
             print("Wrong entry, that boundary condition does not exists")
